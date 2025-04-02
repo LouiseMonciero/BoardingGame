7b67dcc9fd92a17b5744be1d91567ac8a6c18f3c
LouiseMonciero <louisemonciero@gmail.com>
2025-04-02 11:28:48 +0200

    insert_user

---
+++ SQL_scripts/insert_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO User (id_user, name_user, password_user, level_permission) VALUES 
+(0, 'Louise', '12345678', 'user'),
+(1, 'Fafou', '12345678', 'admin'),
+(2, 'Parpaing', '12345678', 'user'),
+(3, 'Tiph', '12345678', 'admin')
+;
