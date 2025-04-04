f5abb80bce84a2e53d660ddd2c95f44a4893a4c9
LouiseMonciero <louisemonciero@gmail.com>
2025-04-04 14:08:59 +0200

    create_bdd_looping_based

--- SQL_scripts/create_table.sql
+++ SQL_scripts/create_table.sql
@@ -12,9 +12,9 @@ CREATE TABLE Categories (
 CREATE TABLE Games (
     id_game INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL CHECK (LENGTH(name) > 0),
-    year INT,
-    URL TEXT,
-    thumbnail TEXT,
+    year_game INT,
+    URL VARCHAR(100),
+    thumbnail VARCHAR(100),
     description TEXT,
     id_category INT,
     boardgamemechanic TEXT,
@@ -24,8 +24,11 @@ CREATE TABLE Games (
     boardgamepublisher TEXT,
     boardgameartist TEXT,
     boardgamedesigner TEXT,
+    id_rules INT NOT NULL,
+    FOREIGN KEY(id_rules) REFERENCES Rules(id_rules)
     FOREIGN KEY (id_category) REFERENCES Category(id_category) ON DELETE CASCADE
 );
+
 
 -- Table Rules
 CREATE TABLE Rules (
@@ -41,32 +44,44 @@ CREATE TABLE Rules (
 );
 
 -- Table Rate
-CREATE TABLE Rates (
-    id_game INT PRIMARY KEY,
-    rank INT,
-    average DECIMAL(4,2) CHECK (average BETWEEN 0 AND 10),
-    bayes_average DECIMAL(4,2) CHECK (bayes_average BETWEEN 0 AND 10),
-    users_rated INT CHECK (users_rated >= 0),
-    FOREIGN KEY (id_game) REFERENCES Game(id_game) ON DELETE CASCADE
+CREATE TABLE Rates(
+   id_game INT,
+   id_rate INT,
+   rank_rate INT,
+   average DECIMAL(15,2),
+   users_rated DECIMAL(15,2),
+   PRIMARY KEY(id_game, id_rate),
+   UNIQUE(id_game),
+   FOREIGN KEY(id_game) REFERENCES Games(id_game)
+);
+
+-- Table Belongs
+CREATE TABLE Belongs(
+    id_game INT,
+    id_category INT,
+    PRIMARY KEY(id_game, id_category),
+    FOREIGN KEY(id_game) REFERENCES Games(id_game),
+    FOREIGN KEY(id_category) REFERENCES Categories(id_category)
 );
 
 -- Table User
 CREATE TABLE Users (
     id_user INT PRIMARY KEY,
-    name_user VARCHAR(100) NOT NULL UNIQUE,
-    password_user VARCHAR(255) NOT NULL,
+    name_user VARCHAR(50) NOT NULL UNIQUE,
+    password_user VARCHAR(50) NOT NULL,
     level_permission VARCHAR(50) NOT NULL CHECK (level_permission IN ('administrator', 'user'))
 );
 
 -- Table Log
-CREATE TABLE Logs (
-    id_log INT PRIMARY KEY,
-    description_log TEXT NOT NULL,
-    id_user INT NOT NULL,
-    id_game INT NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP CHECK (date <= CURRENT_TIMESTAMP),
-    FOREIGN KEY (id_user) REFERENCES User(id_user),
-    FOREIGN KEY (id_game) REFERENCES Game(id_game) -- no ON DELETE CASCADE to keep the logs after the deletions of entries.
+CREATE TABLE Logs(
+    id_game INT,
+    id_user INT,
+    id_log INT,
+    description_log VARCHAR(50),
+    date_log DATE,
+    PRIMARY KEY(id_game, id_user),
+    FOREIGN KEY(id_game) REFERENCES Games(id_game),
+    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
 
