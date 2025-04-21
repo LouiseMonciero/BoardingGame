c46fe25360dfda27d65eaf2b97b2e09d45107c65
LouiseMonciero <louisemonciero@gmail.com>
2025-04-21 17:34:33 +0200

    routes_rates_ok

--- whatdoyouwannaplay/server/routes/rates.js
+++ whatdoyouwannaplay/server/routes/rates.js
@@ -11,21 +11,34 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-// POST /api/games/:id/rate => transaction SQL Transactions_Rate_Game
-router.post('/games/:id/rate', (req, res) => {
-    const id_game = req.params.id;
-    const { id_user, rating } = req.body;
+// POST /api/rates/:id_game => transaction SQL Transactions_Rate_Game (id de game)
+router.post('/:id_game', (req, res) => {
+    const id_game = req.params.id_game;
 
-    db.query('CALL Transactions_Rate_Game(?, ?, ?)', [id_game, id_user, rating], (err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
+    if (req.body === undefined) {
+        res.json({ message: "Body undefined" });
+    } else {
+        const { id_user, rating } = req.body;
+        if (id_user === undefined || rating === undefined) {
+            res.sendStatus(400);
+        } else {
+            db.query('SELECT * FROM View_Games WHERE id_game = ?', [id_game], (err, results) => {
+                if (err) return res.status(500).json({ error: err });
+                if (results.length === 0) return res.status(404).json({ error: 'Jeu introuvable' });
+
+                db.query('CALL Transactions_Rate_Game(?, ?, ?)', [id_game, id_user, rating], (err) => {
+                    if (err) {
+                        return res.status(500).json({ error: err.message });
+                    }
+                    res.status(201).json({ message: 'Évaluation enregistrée' });
+                });
+            });
         }
-        res.status(201).json({ message: 'Évaluation enregistrée' });
-    });
+    }
 });
 
-// DELETE /api/rates/:id => procédure stockée Procedure_Delete_Rate
-router.delete('/rates/:id', (req, res) => {
+// DELETE /api/rates/:id_game => procédure stockée Procedure_Delete_Rate
+router.delete('/:id_game', (req, res) => {
     const id_rate = req.params.id;
 
     db.query('CALL Procedure_Delete_Rate(?)', [id_rate], (err) => {
