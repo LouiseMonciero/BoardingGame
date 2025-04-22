442a316118ca97547a7a2b5903f35cf389b90d45
LouiseMonciero <louisemonciero@gmail.com>
2025-04-22 10:44:06 +0200

    fix

--- whatdoyouwannaplay/server/routes/rates.js
+++ whatdoyouwannaplay/server/routes/rates.js
@@ -7,21 +7,17 @@ const { checkBody } = require('../middlewares');
 router.post('/:id_game', checkBody([id_user, rating]), (req, res) => {
     const id_game = req.params.id_game;
     const { id_user, rating } = req.body;
-    if (id_user === undefined || rating === undefined) {
-        res.sendStatus(400);
-    } else {
-        db.query('SELECT * FROM View_Games WHERE id_game = ?', [id_game], (err, results) => {
-            if (err) return res.status(500).json({ error: err });
-            if (results.length === 0) return res.status(404).json({ error: 'Jeu introuvable' });
+    db.query('SELECT * FROM View_Games WHERE id_game = ?', [id_game], (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        if (results.length === 0) return res.status(404).json({ error: 'Jeu introuvable' });
 
-            db.query('CALL Transactions_Rate_Game(?, ?, ?)', [id_game, id_user, rating], (err) => {
-                if (err) {
-                    return res.status(500).json({ error: err.message });
-                }
-                res.status(201).json({ message: 'Évaluation enregistrée' });
-            });
+        db.query('CALL Transactions_Rate_Game(?, ?, ?)', [id_game, id_user, rating], (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).json({ message: 'Évaluation enregistrée' });
         });
-    }
+    });
 });
 
 // DELETE /api/rates/:id_game => procédure stockée Procedure_Delete_Rate
--- whatdoyouwannaplay/server/routes/users.js
+++ whatdoyouwannaplay/server/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql2');
 const db = require('../config/db');
 const { checkBody } = require('../middlewares');
 
