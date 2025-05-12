7d7402a2828f841ba0dd44f56157ede6ce9b5691
Alexandre Mariotte <alexandre.mariotte@efrei.net>
2025-05-12 11:17:29 +0200

    Update server startup message to include local host URL

--- whatdoyouwannaplay/server/index.js
+++ whatdoyouwannaplay/server/index.js
@@ -58,4 +58,4 @@ app.get('/', (req, res) => {
 
 // Démarrage du serveur
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur Node en écoute sur ${PORT}\n`));
+app.listen(PORT, () => console.log(`Serveur Node en écoute sur ${PORT}\nLocal Host: http://localhost:${PORT}`));
