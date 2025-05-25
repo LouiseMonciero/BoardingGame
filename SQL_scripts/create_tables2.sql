839c06741af5c9721fd11b5ac8657aa07f691b78
LouiseMonciero <louisemonciero@gmail.com>
2025-05-25 15:38:43 +0200

    init_all + Trigger

--- SQL_scripts/create_tables2.sql
+++ SQL_scripts/create_tables2.sql
@@ -1,5 +1,56 @@
--- Triggers to create after the insert of all entries.
+
+-- Création des triggers
+
 DELIMITER //
+CREATE TRIGGER Trigger_Log_Game_Insert
+AFTER INSERT ON Games
+FOR EACH ROW
+BEGIN
+  INSERT INTO Logs (id_game, id_user, description_log, date_log)
+  VALUES (NEW.id_game, NULL, 'Insert game', CURDATE());
+END//
+
+CREATE TRIGGER Trigger_Log_Game_Update
+AFTER UPDATE ON Games
+FOR EACH ROW
+BEGIN
+  INSERT INTO Logs (id_game, id_user, description_log, date_log)
+  VALUES (NEW.id_game, NULL, 'Update game', CURDATE());
+END//
+
+CREATE TRIGGER Trigger_Log_Game_Delete -- FIX // Suppression des clées étrangères dans TALBE Logs
+AFTER DELETE ON Games
+FOR EACH ROW
+BEGIN
+  INSERT INTO Logs (id_game, id_user, description_log, date_log)
+  VALUES (OLD.id_game, NULL, 'Delete game', CURDATE());
+END//
+
+CREATE TRIGGER Trigger_Log_User_Create
+AFTER INSERT ON Users
+FOR EACH ROW
+BEGIN
+  INSERT INTO Logs (id_user, id_game, description_log, date_log)
+  VALUES (NEW.id_user, NULL, 'Create user', CURDATE());
+END//
+
+CREATE TRIGGER Trigger_Log_User_Delete -- FIX // Suppression des clées étrangères dans TALBE Logs
+AFTER DELETE ON Users
+FOR EACH ROW
+BEGIN
+  INSERT INTO Logs (id_user, id_game, description_log, date_log)
+  VALUES (OLD.id_user, NULL, 'Delete user', CURDATE());
+END//
+
+CREATE TRIGGER Trigger_Log_User_PermissionChange
+AFTER UPDATE ON Users
+FOR EACH ROW
+BEGIN
+  IF NEW.level_permission <> OLD.level_permission THEN
+    INSERT INTO Logs (id_user, id_game, description_log, date_log)
+    VALUES (NEW.id_user, NULL, 'Permission changed', CURDATE());
+  END IF;
+END//
 
 CREATE TRIGGER Trigger_Update_Rank
 AFTER INSERT ON Rates
